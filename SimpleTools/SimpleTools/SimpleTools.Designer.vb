121173fc83ccd23b4bdd744b03f21854828822a2
neonxflame <124432647+shermanTWQ@users.noreply.github.com>
2023-12-31 02:48:09 +0800

    Add files via upload

---
+++ SimpleTools/SimpleTools/SimpleTools.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SimpleTools
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShutdown = New System.Windows.Forms.Button()
+        Me.btnRestart = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnShutdown
+        '
+        Me.btnShutdown.Location = New System.Drawing.Point(78, 63)
+        Me.btnShutdown.Name = "btnShutdown"
+        Me.btnShutdown.Size = New System.Drawing.Size(105, 23)
+        Me.btnShutdown.TabIndex = 0
+        Me.btnShutdown.Text = "Shutdown"
+        Me.btnShutdown.UseVisualStyleBackColor = True
+        '
+        'btnRestart
+        '
+        Me.btnRestart.Location = New System.Drawing.Point(78, 92)
+        Me.btnRestart.Name = "btnRestart"
+        Me.btnRestart.Size = New System.Drawing.Size(105, 23)
+        Me.btnRestart.TabIndex = 1
+        Me.btnRestart.Text = "Restart"
+        Me.btnRestart.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(167, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Force System Options"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(66, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(142, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Proceed with caution!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 122)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRestart)
+        Me.Controls.Add(Me.btnShutdown)
+        Me.Name = "Form1"
+        Me.Text = "Simple Tools 1.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnShutdown As Button
+    Friend WithEvents btnRestart As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ SimpleTools/SimpleTools/SimpleTools.vb
@@ -0,0 +1,18 @@
+﻿Public Class SimpleTools
+
+    'Simple Tools 1.0
+    'Provided for non-commercial use. Please refer to Github Repo for more information. 
+    'Contact: Sherman_tan@outlook.com
+    '(C) Tan Wei Qiang Sherman. 
+    Private Sub btnShutdown_Click(sender As Object, e As EventArgs) Handles btnShutdown.Click
+        Process.Start("shutdown", "-s -f -t 00")
+    End Sub
+
+    Private Sub btnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
+        Process.Start("restart", "-r -f -t 00")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
